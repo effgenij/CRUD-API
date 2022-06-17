c956a61eb8e9a692f803228bd0e63ba9ccc21cb4
effgenij <effgenij@gmail.com>
2022-06-17 22:31:57 +0300

    Added enums

---
+++ src/helpers/enums.ts
@@ -0,0 +1,16 @@
+export enum StatusCodes {
+    ok = 200,
+    created = 201,
+    deleted = 204,
+    badRequest = 400,
+    notFound = 404,
+    serverError = 500
+}
+
+export enum Messages {
+    created = '',
+    deleted = '',
+    badRequest = '',
+    notFound = '',
+    serverError = ''
+}
