3e40d3aa858d33718af5f904d2597511f07e5450
effgenij <effgenij@gmail.com>
2022-06-19 00:23:25 +0300

    add create user

--- src/controllers/user_controller.ts
+++ src/controllers/user_controller.ts
@@ -1,14 +1,27 @@
 import * as User from '../models/user_model';
 import type { IncomingMessage, ServerResponse } from 'http';
-import { Messages, Codes } from '../helpers/types';
+import { Messages, Codes, IUser, paramsTuple } from '../helpers/types';
 
 async function getUsers(req: IncomingMessage, res: ServerResponse): Promise<void>{
   try{
     const users = await User.all();
     createResponse(res, Codes.ok, users);
-  } catch (err) {
-        console.error(err);
+  } catch (error) {
+    createResponse(res, Codes.serverError, { message: "server error" });
+    return;
   }
+}
+
+async function createUser(req: IncomingMessage, res: ServerResponse): Promise<void>{
+    try {
+        const params = await parseParams(req);
+        const user = await User.create(params);
+        createResponse(res, Codes.ok, user);
+      } catch (error) {
+        createResponse(res, Codes.serverError, { message: "server error" });
+        return;
+      }
+    
 }
 
 function createResponse(res: ServerResponse, statusCode: number, value: any ): void{
@@ -16,4 +29,38 @@ function createResponse(res: ServerResponse, statusCode: number, value: any ): v
     res.end(JSON.stringify(value)); 
 }
 
-export {getUsers};
+function parseRequest (req: IncomingMessage): Promise<IUser> {
+    return new Promise((resolve, reject) => {
+        try {
+            let body = '';
+            req.on('data', (chunk) => {
+              body += chunk.toString();
+            });
+            req.on('end', async () => {
+              resolve(JSON.parse(body));
+            });
+          } catch (error) {
+            throw new Error();
+          }
+    });
+  }
+
+  async function parseID (req: IncomingMessage): Promise<string> {
+    try{
+      const body = await parseRequest(req);
+      return body.id;
+    } catch (error) {
+        throw new Error();
+    }
+  }
+
+  async function parseParams (req: IncomingMessage):Promise<paramsTuple> {
+    try{
+      const body = await parseRequest(req);
+      return [body.username, body.age, body.hobbies];
+    } catch (error) {
+        throw new Error();
+    }
+  }
+
+export {getUsers, createUser};
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import http from 'http';
-import { getUsers } from './controllers/user_controller';
+import { getUsers, createUser } from './controllers/user_controller';
 
 const server = http.createServer((req, res)=> {
     if(req.url === '/api/users' && req.method === 'GET') {
         getUsers(req, res);
-    } else {
+    } else if(req.url === '/api/users' && req.method === 'POST') {
+        createUser(req, res);
+    }else {
         res.writeHead(404, { 'Content-type': 'application/json' });
         res.end(JSON.stringify({ message: 'Route not found' }));
     }
