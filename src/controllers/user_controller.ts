b8a2a138e4b23ed92741e835b65a353db77951da
effgenij <effgenij@gmail.com>
2022-06-19 00:52:21 +0300

    Add other controller functions

--- src/controllers/user_controller.ts
+++ src/controllers/user_controller.ts
@@ -8,23 +8,51 @@ async function getUsers(req: IncomingMessage, res: ServerResponse): Promise<void
     const users = await User.all();
     createResponse(res, Codes.ok, users);
   } catch (error) {
-    createResponse(res, Codes.serverError, { message: 'server error' });
-    
+    createResponse(res, Codes.serverError, { message: 'server error' });  
   }
 }
 
 async function createUser(req: IncomingMessage, res: ServerResponse): Promise<void>{
-    try {
-        const params = await parseParams(req);
-        const user = await User.create(params);
-        createResponse(res, Codes.ok, user);
-      } catch (error) {
-        createResponse(res, Codes.serverError, { message: 'server error' });
-        
-      }
-    
+  try {
+    const params = await parseParams(req);
+    const user = await User.create(params);
+    createResponse(res, Codes.ok, user);
+  } catch (error) {
+    createResponse(res, Codes.serverError, { message: 'server error' });   
+  }   
+}
+
+async function findUser(req: IncomingMessage, res: ServerResponse): Promise<void>{
+  try {
+    const id = await parseID(req);
+    const user = await User.find(id);
+    createResponse(res, Codes.created, user);
+  } catch (error) {
+    createResponse(res, Codes.serverError, { message: 'server error' });   
+  }   
+}
+
+async function updateUser(req: IncomingMessage, res: ServerResponse): Promise<void>{
+  try {
+    const id = await parseID(req);
+    const params = await parseParams(req);
+    const user = await User.update(id, params);
+    createResponse(res, Codes.ok, user);
+  } catch (error) {
+    createResponse(res, Codes.serverError, { message: 'server error' });   
+  }   
+}
+
+async function deleteUser(req: IncomingMessage, res: ServerResponse): Promise<void>{
+  try {
+    const id = await parseID(req);
+    await User.destroy(id);
+    createResponse(res, Codes.created, { message: 'user delted' });
+  } catch (error) {
+    createResponse(res, Codes.serverError, { message: 'server error' });   
+  }   
 }
 
 
 
-export { getUsers, createUser };
+export { getUsers, createUser, findUser, updateUser, deleteUser };
